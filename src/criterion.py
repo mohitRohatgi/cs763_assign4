da963cd7ecf0d2a075ea696709cd5d8643d92846
Mohit Rohatgi <mohit.rohatgi.mec10@itbhu.ac.in>
2018-03-22 03:29:40 +0530

    refactoring

--- src/criterion.py
+++ src/criterion.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 
 class Criterion:
     def forward(self, input_vec, target):
-        batch_size, num_classes = input_vec._shape
+        batch_size, num_classes = input_vec.get_shape().as_list()
         one_hot_target = tf.one_hot(target, num_classes)
         return tf.losses.sigmoid_cross_entropy(one_hot_target, input_vec)
 
